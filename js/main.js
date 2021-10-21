dea6ae1584b1f19a682c08e0281f54c7fc25a3b1
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-21 16:31:44 +0200

    consol-log-exit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 //Numero di chilometri
 const numberKm = parseInt( prompt('Inserisci il numero di km del tuo viaggio') );
-console.log(numberKm);
 
 //Verifica km
 if (isNaN(numberKm)){
@@ -8,33 +7,23 @@ if (isNaN(numberKm)){
 }
 //Età passeggero
 const age1 = parseInt( prompt('Inserisci la tua età') );
-console.log(age1);
 
 //Verifica km
 if (isNaN(age1) || (age1 > 120)){
     alert('Errore, ricarica la pagina');
 }
 
-/**Calcolo prezzo del viaggio
- *   1-Ogni km costa 0,21€
- *   2-Sconto 20% per i minorenni
- *   3-Sconto 40% per gli over 65
- */
-
 //Prezzo Viaggio
 let price1 = (numberKm) * (0.21);
-console.log(price1 + '€');
 
 //Sconto minorenni
 if (age1 < 18){
     price1 -= (price1 * 20) / 100;
-    console.log(price1)
 }
 
 //Sconto maggiorenni
 if (age1 > 65){
     price1 -= (price1 * 40) / 100;
-    console.log(price1)
 }
 
 
@@ -42,8 +31,7 @@ if (age1 > 65){
 //Calcolo prezzo umano
 let price2 = price1;
 let price = price2.toFixed(2)
-console.log(price)
 
 //Visualizzazione Prezzo
-document.getElementById('price') .innerHTML = `Il prezzo del tuo biglietto è: ${price}`;
+document.getElementById('price') .innerHTML = `Il prezzo del tuo biglietto è: ${price}€`;
 
