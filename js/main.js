9cf676f24c7c68050e412e65a0d77ea879c24c6e
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-21 15:34:09 +0200

    età-passeggero

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 //Numero di chilometri
-const numberKm = perseInt(prompt('Inserisci il numero di km del tuo viaggio'));
+const numberKm = parseInt( prompt('Inserisci il numero di km del tuo viaggio') );
 console.log(numberKm);
 
+//Verifica km
+if (isNaN(numberKm)){
+    alert('Errore, ricarica la pagina');
+}
 //Età passeggero
+const age1 = parseInt( prompt('Inserisci la tua età') );
+console.log(age1);
 
 /**Calcolo prezzo del viaggio
  *   1-Ogni km costa 0,21€
