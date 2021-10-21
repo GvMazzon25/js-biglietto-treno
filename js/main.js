46ae7688a2d3bf8f5e13500f2ebf7f4f81fe5e3c
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-21 15:23:46 +0200

    start-project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Numero di chilometri
+const numberKm = perseInt(prompt('Inserisci il numero di km del tuo viaggio'));
+console.log(numberKm);
+
+//Età passeggero
+
+/**Calcolo prezzo del viaggio
+ *   1-Ogni km costa 0,21€
+ *   2-Sconto 20% per i minorenni
+ *   3-Sconto 40% per gli over 65
+ */
