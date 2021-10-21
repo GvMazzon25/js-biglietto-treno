a2b5ceaac77fe0f0706b7ed793b1387bf88a4349
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-21 15:50:02 +0200

    prezzo-viaggio

--- js/main.js
+++ js/main.js
@@ -10,8 +10,16 @@ if (isNaN(numberKm)){
 const age1 = parseInt( prompt('Inserisci la tua età') );
 console.log(age1);
 
+//Verifica km
+if (isNaN(age1) || (age1 > 120)){
+    alert('Errore, ricarica la pagina');
+}
+
 /**Calcolo prezzo del viaggio
  *   1-Ogni km costa 0,21€
  *   2-Sconto 20% per i minorenni
  *   3-Sconto 40% per gli over 65
  */
+
+//Prezzo Viaggio
+
